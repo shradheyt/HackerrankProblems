c956684f29b4dd31c0c9d8c065332b29bef07a0d
shradheyt <shradhey029tripathi@gmail.com>
2017-08-16 23:11:02 +0530

    Compare the Triplets Solution

---
+++ Algorithm_Basic_CompareTheTriplets.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        // Complete this function
+        int[] array = new int[2];
+        array[0]=0;
+        array[1]=0;
+        if(a0 > b0)
+            array[0]+=1;
+        else if(a0 < b0)
+            array[1]+=1;
+        
+        if(a1 > b1)
+            array[0]+=1;
+        else if(a1 < b1)
+            array[1]+=1;
+        
+        if(a2 > b2)
+            array[0]+=1;
+        else if(a2 < b2)
+            array[1]+=1;
+        
+        return array;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
